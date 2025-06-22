30947ab788091d1a898c7d2c74843eca5f36cd10
Thierry Staakman <staakman@OpenSuse.domain>
2025-06-22 18:38:36 +0200

    added tools directory

---
+++ tools/install-hyprland.sh
@@ -0,0 +1,21 @@
+sudo zypper install --no-recommends \
+	hyprland \
+	hyprland-qtutils \
+	hyprland-wallpapers \
+	waybar \
+	wofi \
+	xdg-desktop-portal-hyprland \
+	xdg-desktop-portal-wlr \
+	grim \
+	slurp \
+	wl-clipboard \
+	mako \
+	hyprpaper \
+	kitty \
+	thunar \
+	pavucontrol \
+	brightnessctl \
+	rofi-network-manager \
+	hyprlock \
+	hypridle \
+	playerctl
